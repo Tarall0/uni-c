58593d4382f93438b69d46855a638102585122c8
Tarallo <98182571+Tarall0@users.noreply.github.com>
2024-01-09 13:02:41 +0100

    Update coloredPointsExample.c
    adding switch case to actually print a collared text for the color information

--- coloredPointsExample.c
+++ coloredPointsExample.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 // Defining data structure {enum, struct and array constructor(s)}
-typedef enum {green, red, blue, black, white} color;
+typedef enum {green, white, blue, red, black} color;
 
 typedef struct {
     float x, y;
@@ -12,6 +12,39 @@ typedef struct {
 
 typedef point set[3];
 
+// Considering the values to print in the terminal colored text
+// red \033[1;31m
+// green \033[1;32m
+// blue \033[1;34m
+// black \033[1;30m
+// white \033[1;37m
+
+// Function to print color based on the enum value
+void printColor(color c) {
+    switch (c) {
+        case green:
+            printf("\033[1;32m"); // Green color code
+            break;
+        case red:
+            printf("\033[1;31m"); // Red color code
+            break;
+        case blue:
+            printf("\033[1;34m"); // Blue color code
+            break;
+        case black:
+            printf("\033[1;30m"); // Black color code
+            break;
+        case white:
+            printf("\033[1;37m"); // White color code
+            break;
+        default:
+            // Default color (reset)
+            printf("\033[0m");
+            break;
+    }
+}
+
+
 int main() {
 
     int i, j;
@@ -19,7 +52,6 @@ int main() {
     float distance, distanceX, distanceY;
 
     // Initialize randomly
-
     for(i = 0; i < 3; i = i+1){
         // Casting
         s[i].x = rand()/(float)RAND_MAX;
@@ -33,10 +65,26 @@ int main() {
             distanceX = (s[i].x - s[j].x)*(s[i].x - s[j].x);
             distanceY = (s[i].y - s[j].y)*(s[i].y - s[j].y);
             distance = sqrt(distanceX + distanceY);
-            printf("\nPoint I (%.4f, %.4f - Color: %.d) - Point J (%.4f, %.4f - Color: %.d): \n", s[i].x, s[i].y, s[i].c, s[j].x, s[j].y, s[j].c);
+
+            printf("\nPoint I (%.4f, %.4f - Color: ", s[i].x, s[i].y);
+            // Print the colored color number for point i
+            printColor(s[i].c);
+            printf("%.d", s[i].c);
+            // Reset color
+            printf("\033[0m");
+            printf(") - ");
+
+            printf("Point J (%.4f, %.4f - Color:", s[j].x, s[j].y);
+            // Print the colored color number for point j
+            printColor(s[j].c);
+            printf("%.d", s[j].c);
+            // Reset color
+            printf("\033[0m");
+            printf(") - ");
             printf(" -> Distance: %.4f\n\n", distance);
         }
     }
-    
+
     return 0;
 }
+
